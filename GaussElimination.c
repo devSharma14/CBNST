4b36cbf6a82eb4efe63f627a8b264523abd2747d
devSharma14 <devs140124@gmail.com>
2024-10-05 15:41:33 +0530

    gauss elimination updation

--- GaussElimination.c
+++ GaussElimination.c
@@ -26,20 +26,22 @@ int main() {
 
     // Step 2 : Convert it into upper triangular matrix
     for(int i=0; i<n; i++) {
-        if(fabs(mat[i][i]) < 1e-6) {
-            printf("Mathematical error! Division by zero or nearly zero pivot encountered. \n");
-            return -1;
-        }
-        for(int row=i+1; row<n; row++) {
-            float coeff = mat[row][i] / mat[i][i];
-            for(int col=0; col<n+1; col++) {
-                mat[row][col] = mat[row][col] - (coeff * mat[i][col]);
-                if(mat[row][col] == -0.0) {
-                    mat[row][col] = 0.0;
+        for(int j=0; j<n; j++) {
+            if(j > i) {
+                float coeff = mat[j][i] / mat[i][i];
+                for(int k=0; k<n+1; k++) {
+                    mat[j][k] = mat[j][k] - coeff * mat[i][k];
                 }
             }
         }
-   }
+        printf("Intermediate form : \n");
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n+1; j++) {
+                printf("%f \t", mat[i][j]);
+            }
+            printf("\n");
+        }
+    }
    printf("\nUpper triangular matrix : \n");
    for(int i=0; i<n; i++) {
         for(int j=0; j<n+1; j++) {
